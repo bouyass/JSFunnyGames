aa5b3028707565cd439a7f31c5ce671b1c84b77e
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-08-29 00:20:57 +0200

    listeners implementation

--- memoryGame/index.js
+++ memoryGame/index.js
@@ -4,6 +4,9 @@ var imageIndexs = [0,1,2,3,4,5,6,7];
 var cellIndexs = [0,1,2,3,4,5,6,7];
 var imageElements = [];
 var rootElement = document.getElementById('memoGame');
+var result = document.getElementById('result');
+var imagesReturned = undefined;
+var score = 0;
 
 document.addEventListener("DOMContentLoaded", (event) => {
     initGame();
@@ -13,7 +16,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
 const bindListener = function(i, element){
     element.addEventListener('click',function(){
         element.setAttribute('src',`images/image-${board[i][1]}.png`);
-    });
+        if(imagesReturned !== undefined){
+            if(board[imagesReturned][1] === board[i][1]){
+                score++;
+                result.textContent = score;
+                element.setAttribute('src',`images/image-${board[imagesReturned][1]}.png`);
+                document.getElementById(imagesReturned).setAttribute('src',`images/image-blank.png`);
+                document.getElementById(i).setAttribute('src',`images/image-blank.png`);
+                imagesReturned = undefined; 
+            }else{
+                document.getElementById(imagesReturned).setAttribute('src',`images/image-back.png`);
+                document.getElementById(i).setAttribute('src',`images/image-back.png`);
+                imagesReturned = undefined;
+            }
+        }else{
+            imagesReturned = i;
+        }
+    }); 
 };
 
 
@@ -38,7 +57,7 @@ const printBoard = function(){
     let element = undefined;
     for(let i=0;i<board.length;i++){
             element = document.createElement('img');
-            element.setAttribute('src',`images/image-blank.png`);
+            element.setAttribute('src',`images/image-back.png`);
             element.setAttribute('id',`${i}`);
             imageElements.push(element);
             bindListener(i, element);
