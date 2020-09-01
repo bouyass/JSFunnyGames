ce22a9a25b9b90844175156a56ebc4b036a7344b
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-09-01 16:13:03 +0200

    horizontal and vertical check

--- connect_four/index.js
+++ connect_four/index.js
@@ -6,8 +6,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     initGame(); 
 });
 
+// creating the board 
 const initGame = function(){
-    // creating the board 
+ 
     for(let i =0; i<size;i++){
         tr = document.createElement('tr');
         tr.setAttribute('id',i);
@@ -22,18 +23,76 @@ const initGame = function(){
     }
 }
 
+// add click listeners to table cells 
 const addListener = function(element){
+    let id = element.getAttribute('id');
+    let indexs = id.split('-');
     element.addEventListener('click', (event) => {
         if(turn === 'green'){
-
+            if(element.children.length === 0){
+                let greenCircle = document.createElement('img');
+                greenCircle.setAttribute('src','images/green.png');
+                greenCircle.setAttribute('id',`${indexs[1]}-${indexs[2]}-green`);
+                element.appendChild(greenCircle);
+                turn = "red";
+            }
         }else{
-
+            if(element.children.length === 0){
+                let redCircle = document.createElement('img');
+                redCircle.setAttribute('src','images/red.png');
+                redCircle.setAttribute('id',`${indexs[1]}-${indexs[2]}-red`);
+                element.appendChild(redCircle);
+                turn = "green";
+            }
         }
-        checkGame();
+        checkGame(turn === 'green' ? "red" : "green");
     })
 }
 
 
-const checkGame = function (){
+const checkGame = function (turn){
 
+    let serie = 0;
+
+    // horizontal check
+    for (let l = 0; l<size;l++){
+        for(let m = 0;m<size;m++){
+            if(document.getElementById(`${l}-${m}-${turn}`) !== null){
+                serie++;
+                // if 4 balls aligned, win
+                if(serie === 4){
+                    alert("The "+turn+" player won !!");
+                    reset();
+                }
+            }else{
+                serie = 0;
+            }
+        }
+    }
+
+    // vertical check
+    for (let l = 0; l<size;l++){
+        for(let m = 0;m<size;m++){
+            if(document.getElementById(`${m}-${l}-${turn}`) !== null){
+                serie++;
+                // if 4 balls aligned, win
+                if(serie === 4){
+                    alert("The "+turn+" player won !!");
+                    reset();
+                }
+            }else{
+                serie = 0;
+            }
+        }
+    }
+    
+}
+
+const reset = function(){
+    for (let l = 0; l<size;l++){
+        for(let m = 0;m<size;m++){
+            document.getElementById(`row-${l}-${m}`).innerHTML = '';   
+        }
+    }
+    turn = "green";
 }
